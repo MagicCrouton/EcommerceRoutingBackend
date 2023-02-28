f4441d96f274e0a34498db10ecebbda400fd71a9
Eugene Park <e.park5336@gmail.com>
2023-02-28 18:36:32 -0500

    started adding category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,28 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.json(await Category.findAll({
+    include: [{model: Product}]
+  }))
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  res.json(await Category.findByPk(req.params.id , {
+    include: [{model: Product}]
+  }))
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  let newCategory = await Category.create({
+    category_name: req.body.category_name
+  })
+  res.json(newCategory)
 });
 
 router.put('/:id', (req, res) => {
